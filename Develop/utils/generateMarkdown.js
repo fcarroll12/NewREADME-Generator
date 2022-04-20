a3bc9c01fe8753e6005d7e6ed7204e2148d88cda
Fiona Carroll <fiona.carroll@me.com>
2022-04-20 14:53:45 -0700

    Initial code

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,9 @@
+// TODO: Add code to generate markdown for README given a data object parameter containing the user input.
+function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
