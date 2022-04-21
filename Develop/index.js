29e15ca0d7aefdd7910113d9ef691c3ffe82d010
Fiona Carroll <fiona.carroll@me.com>
2022-04-20 19:39:03 -0700

    index.js completed and working on badges

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,68 @@
 // TODO: Include packages needed for this application
 
 // TODO: import the generateMarkdown function from utils/generateMarkdown.js
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Write code to get user input, generate markdown, and save it to a file.
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What is required to install your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are some instructions for usage?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license does your project use?',
+        choices: ['MIT', 'GNU', 'Apache','Mozilla', 'BSD']
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Were there any collaborators or contributors to your project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you run tests for your application?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address where users can reach you with additional questions?',
+      },
+    ])
+    .then((response) => {
+      const readmeContent = generateMarkdown(response);
+  
+      fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.log(err) : console.log('You have succesfully created a README!')
+      );
+    });
+}
+
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,43 @@
 // TODO: Add code to generate markdown for README given a data object parameter containing the user input.
-function generateMarkdown(data) {
+function createLicenseBadge(license) {
+    return `# ${}`
+}
+
+function generateMarkdown(data) { 
     return `# ${data.title}
-  
+
+    ## Table of Contents: 
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Credits](#Credits)
+    - [License](#License)
+    - [Contributing](#Contributing)
+    - [Tests](#Tests)
+    - [Questions](#Questions)
+
+    ## Description
+    ${data.description}
+
+    ##Installation 
+    ${data.installation}
+
+    ##Usage
+    ${data.usage}
+
+    ##License
+    ${createLicenseBadge(data.license)}
+
+    ##Contributing 
+    ${data.contributing}
+
+    ##Tests 
+    ${data.tests}
+
+    ##Questions
+    If you have any additional questions feel free to contact me at:
+    [GitHub](https://github.com/${data.github}) or by email at ${data.email}
+    
   `;
   }
   
